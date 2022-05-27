d66b83d1187ca4e3a65152ce75d8dd444c2a6cd9
Aleksandr Repetskyi <aleksandr@cidersecurity.io>
2022-05-27 16:20:36 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ class HelloWorld
 {
         public function sayHello(DateTimeImutable $date): void
         {
-                echo 'Hello, ' . $date->format('j. n. Y');
+                echo 'Hello, 1' . $date->format('j. n. Y');
         }
 }
 
